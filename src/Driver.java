de047bdce1e8257c17d9283a68c7db654885471b
Iwan Siauw <evansiauw@gmail.com>
2018-03-21 15:27:28 -0400

    Added Descriptions

--- src/Driver.java
+++ src/Driver.java
@@ -1,8 +1,18 @@
+/* Jobs come into the system, arrivaling with an exponential interarrival time 
+ * (mean 13 milliseconds) , and have an exponential amount of work with 
+ * a mean of 10 milliseconds. They wait their turn for service in FCFS order. 
+ * Once they have the attention of the server, they are served 
+ * but for not more than 5 miliseconds. If their required work is not completed 
+ * they go to the back of the jobs waiting for service. 
+
+ * Run the system for 100 seconds. 
+ * Output the amount of time untill completion for each 1000 jobs.
+*/
 
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 
 	}
 
