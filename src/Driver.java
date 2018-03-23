6e567f716321f4bbc618418a3a81cd4240643cdb
JoshGoldstein <jgoldstein6796@gmail.com>
2018-03-22 20:43:09 -0400

    added job class

--- src/Driver.java
+++ src/Driver.java
@@ -1,18 +1,38 @@
-/* Jobs come into the system, arrivaling with an exponential interarrival time 
+import java.util.ArrayList;
+import java.util.Random;
+
+/* Jobs come into the system, arriving with an exponential interarrival time 
  * (mean 13 milliseconds) , and have an exponential amount of work with 
  * a mean of 10 milliseconds. They wait their turn for service in FCFS order. 
  * Once they have the attention of the server, they are served 
- * but for not more than 5 miliseconds. If their required work is not completed 
+ * but for not more than 5 milliseconds. If their required work is not completed 
  * they go to the back of the jobs waiting for service. 
 
  * Run the system for 100 seconds. 
- * Output the amount of time untill completion for each 1000 jobs.
+ * Output the amount of time until completion for each 1000 jobs.
 */
 
 public class Driver {
 
 	public static void main(String[] args) {
-
+	    ArrayList<Job> jobs = new ArrayList<>();
+	    int numOfJobs = 1000;
+	    int numOfJobsCompleted = 0;
+	    for(int i = 1; i <= numOfJobs; i++){
+	        Job j = new Job(i, i*13, 10); 
+	        jobs.add(j);
+	    }
+	    int jobBeingServed = 1;
+	    while (numOfJobsCompleted != numOfJobs && jobBeingServed != 999){
+	        Job j = jobs.get(jobBeingServed-1);
+	        Random random = new Random();
+	        j.amountOfWork =- random.nextInt(5 - 1 + 1) + 1; // jobs are served for 1-5 ms
+	        if (j.amountOfWork <= 0){
+	            numOfJobsCompleted++;
+	            System.out.println("job " + j.jobNumber + " completed");
+	        }
+	        else jobBeingServed++;
+	    }
 
 	}
 
---
+++ src/Job.java
@@ -0,0 +1,11 @@
+public class Job {
+    int jobNumber;
+    int arrivalTime;
+    int amountOfWork;
+    
+    public Job(int jNo, int aTime, int aWork){
+        jobNumber = jNo;
+        arrivalTime = aTime;
+        amountOfWork = aWork;
+    }
+}
